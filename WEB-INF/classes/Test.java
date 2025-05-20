a5813625b270913fcbfa0efd0e10945158772c5b
shivasrimaddela <shivasrimaddela866@gmail.com>
2025-05-20 14:44:09 +0530

    updated java

--- WEB-INF/classes/Test.java
+++ WEB-INF/classes/Test.java
@@ -6,15 +6,12 @@ public class Test extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        // Set response content type
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        // Get username and password from form
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        // Get session and initialize login count
         HttpSession session = request.getSession();
         Integer count = (Integer) session.getAttribute("loginCount");
         if (count == null) {
@@ -24,7 +21,6 @@ public class Test extends HttpServlet {
         }
         session.setAttribute("loginCount", count);
 
-        // Display response
         out.println("<html><body>");
         out.println("<h2>Hello, " + username + "!</h2>");
         out.println("<p>You have logged in " + count + " times during this session.</p>");
